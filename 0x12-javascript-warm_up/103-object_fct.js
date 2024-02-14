503aa6940ba3ce33a7c8f9e8d5d5d3878a315a5a
Funminiyi Peter <funminiyiop@gmail.com>
2024-02-14 18:42:51 +0100

    modify warmups

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,16 +2,16 @@
 const myObject = {
   type: 'object',
   value: 12
-};
-console.log(myObject);
+}
+console.log(myObject)
 
 myObject.incr = function () {
-  this.value++;
-};
+  this.value++
+}
 
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+myObject.incr()
+console.log(myObject)
+myObject.incr()
+console.log(myObject)
+myObject.incr()
+console.log(myObject)
