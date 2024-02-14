9bea5fbab81476e1d7d4eb2d46671b6f5dd988a2
Funminiyi Peter <funminiyiop@gmail.com>
2024-02-14 17:10:00 +0100

    correcting the 0x12-javascript-warm_up task files

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,9 @@
 
 // script to print strings using a loop
 
-let i;
-const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 
 for (i = 0; i < 3; i++) {
-  console.log(myString[i]);
+  console.log(myString[i])
 }
