b5fbd8cea9a220ed34595be4a4041d49b71cdf4f
Funminiyi Peter <funminiyiop@gmail.com>
2024-02-14 14:58:06 +0100

    another correction on task files for 0x12-javascript-warm_up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-const arg = parseInt(process.argv[2]);
-
-if (arg) {
-  console.log('My number: ' + arg);
-} else {
-  console.log('Not a number');
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
